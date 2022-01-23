a300a1286a99068d41ef9498dcc2f823663930fa
cnvorous <cnvorous@gmail.com>
2022-01-23 17:22:40 +0000

    Deck of Cards

--- src/app.js
+++ src/app.js
@@ -2,10 +2,25 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let randomNumber = Math.floor(Math.random() * 12);
+
+  let randomSuit = Math.floor(Math.random() * 4);
+
+  let numbers = [2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K", "A"];
+  let suits = ["&#9824", "&#9827", "&#9829", "&#9830"];
+
+  document.querySelector(".card-number").innerHTML = numbers[randomNumber];
+  document.querySelector(".top-suit").innerHTML = suits[randomSuit];
+  document.querySelector(".bottom-suit").innerHTML = suits[randomSuit];
+
+  if (suits[randomSuit] == "&#9829" || suits[randomSuit] == "&#9830") {
+    document.querySelector(".top-suit").style.color = "red";
+    document.querySelector(".bottom-suit").style.color = "red";
+    document.querySelector(".card-number").style.color = "red";
+  } else {
+    document.querySelector(".top-suit").style.color = "black";
+    document.querySelector(".bottom-suit").style.color = "black";
+    document.querySelector(".card-number").style.color = "black";
+  }
 };
